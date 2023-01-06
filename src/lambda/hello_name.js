2b6bbea28818d1cedaf004647058832103a58085
Kshateesh <kshateesh007@gmail.com>
2023-01-06 12:54:37 +0530

    Update hello_name.js

--- src/lambda/hello_name.js
+++ src/lambda/hello_name.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
   const name = event.queryStringParameters.name || "World";
-
+  console.log("log from hello name");
   return {
     statusCode: 200,
     body: `Hello, ${name}`
